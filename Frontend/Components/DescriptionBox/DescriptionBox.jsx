323de34b2f0520519bc0adf99f5b598928c43b70
Yukti <162004395+yukti-singhal@users.noreply.github.com>
2024-08-08 11:41:20 +0530

    Add files via upload

---
+++ Frontend/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import './DescriptionBox.css';
+
+const DescriptionBox = ()=> {
+    return (
+        <div className="descriptionbox">
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>An ecommerce website is an online store where customers can find products, browse offerings, and place purchases online. It facilitates the transaction between a buyer and seller. A digital storefront can serve as the virtual equivalent of the product shelves, sales staff, and cash register of a physical shop.
+                </p>
+                <p>
+                An ecommerce website is an online store where customers can find products, browse offerings, and place purchases online. It facilitates the transaction between a buyer and seller. A digital storefront can serve as the virtual equivalent of the product shelves, sales staff, and cash register of a physical shop.
+                </p>
+                
+            </div>
+        
+        </div>
+    )
+}
+
+export default DescriptionBox;
